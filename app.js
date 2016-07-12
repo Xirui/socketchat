d3ebbaf9404fc7a576b128f953151143de4cbf2c
Xirui <xirui1@outlook.com>
2016-07-12 10:17:52 +0800

    change to bot

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var restify = require('restify');
+var builder = require('botbuilder');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-    io.emit('chat message', msg + ' -- Auto response from server!');
- });
-});
-http.listen(port, function(){
-    console.log('listening on ' + port);
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: process.env.BOTFRAMEWORK_APPID, 
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+};
+
+// Create bot
+var bot = new builder.BotConnectorBot(botConnectorOptions);
+bot.add('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
 });
 
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
